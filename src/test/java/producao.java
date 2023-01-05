ea055af23ace050cf3084aeeeddc66186355a052
brunofranklin <bruno_franklin@msn.com>
2023-01-05 10:54:20 -0300

    Refatorando cṕdigo

--- src/test/java/producao.java
+++ src/test/java/producao.java
@@ -1,46 +1,68 @@
 import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 
 public class producao {
+    WebDriver nav = new ChromeDriver();
 
-    @Test
-    public void testScreen() {
+    @BeforeClass
+    public void testScreen(){
         // System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
-        WebDriver nav = new ChromeDriver();
+        //WebDriver nav = new ChromeDriver();
         nav.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         nav.manage().window().maximize();
         nav.get("https://app.pulsus.mobi");
         nav.findElement(By.id("identifier")).click();
-        nav.findElement(By.id("identifier")).sendKeys("");
+        nav.findElement(By.id("identifier")).sendKeys(" timeqa@pulsus.mobi");
         nav.findElement(By.id("password")).click();
-        nav.findElement(By.id("password")).sendKeys("");
+        nav.findElement(By.id("password")).sendKeys("QAbggi123");
         nav.findElement(By.id("action")).click();
         nav.findElement(By.xpath("//*[@id=\"react-ae-recommended-modal\"]/div/div/div[2]/span")).click();
-        //DashBoard
-        nav.findElement(By.cssSelector(".nav-item-dashboard .nav-item-label")).click();
-        nav.findElement(By.cssSelector(".dashboard-header")).click();
-        //Consumo de Dados
+    }
+
+    @AfterClass
+    public void logoff () {
+        nav.findElement(By.cssSelector(".dropdown:nth-child(4) > a svg")).click();
+        nav.findElement(By.linkText("Sair")).click();
+    }
+    @Test
+    public void Dashboard() {
+         nav.findElement(By.cssSelector(".nav-item-dashboard .nav-item-label")).click();
+         nav.findElement(By.cssSelector(".dashboard-header")).click();
+    }
+    @Test
+    public void ConsumoDeDados() {
         nav.findElement(By.cssSelector("nav > .nav-item:nth-child(2) .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".content-title")).getText(), "Consumo de Dados");
-        //Mapa de Localização
+    }
+    @Test
+    public void Localizacao() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(3) > a > .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".content-title")).getText(), "Mapa de localizações");
-        //Track
+    }
+    @Test
+    public void Track() {
         nav.findElement(By.cssSelector("nav > .nav-item:nth-child(4) > a")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".map-sidebar-header > h3")).getText(), "Track");
-        //Pulsus Insight
+    }
+    @Test
+    public void PulsusInsight() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(5) .nav-item-label")).click();
-        //Geofence
+    }
+    @Test
+    public void Geofence() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(6) .nav-item-label")).click();
-        //Dispositivos e paginas internas
+    }
+    @Test
+    public void DispositivosPaginasInternas() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(8) .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Dispositivos");
         nav.switchTo().frame("devices-new-iframe");
@@ -51,18 +73,26 @@ public class producao {
         nav.findElement(By.cssSelector(".nav-item:nth-child(4) > .nav-link")).click();
         nav.findElement(By.id("tab-phone-calls-load")).click();
         nav.findElement(By.cssSelector(".nav-item:nth-child(6) > .nav-link")).click();
-        //Grupos
+    }
+    @Test
+    public void Grupos() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(9) .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Grupos");
-        //Aplicativos
+    }
+    @Test
+    public void Aplicativos() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(10) .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Aplicativos");
-        //Launchers
+    }
+    @Test
+    public void Launchers() {
         nav.findElement(By.cssSelector(".launcher-nav .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Launchers");
         nav.findElement(By.cssSelector("tr:nth-child(13) .media a")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".launcher-name")).getText(), "teste_Gabi");
-        //Configurações
+    }
+    @Test
+    public void Configuracoes() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(2) > .div .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Configurações");
         nav.findElement(By.cssSelector("tr:nth-child(1) .media span")).click();
@@ -73,27 +103,22 @@ public class producao {
         nav.findElement(By.cssSelector(".nav-item:nth-child(5) > .nav-link")).click();
         nav.findElement(By.cssSelector(".nav-item:nth-child(6) > .nav-link")).click();
         nav.findElement(By.cssSelector(".nav-item:nth-child(7) > .nav-link")).click();
-        //Arquivos
+    }
+    @Test
+    public void Arquivos() {
         nav.findElement(By.cssSelector(".nav-item:nth-child(3) > .div .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Arquivos");
-        //Enterprise
+    }
+    @Test
+    public void Enterprise() {
         nav.findElement(By.cssSelector(".nav-category-body > .nav-item:nth-child(4) .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".info:nth-child(1) > .info-label")).getText(), "ENTERPRISE ID");
-        //Configurações IOS
+    }
+    @Test
+    public void ConfiguracoesIos() {
         nav.findElement(By.cssSelector(".nav-category:nth-child(12) .div .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector(".datagrid-title > h3")).getText(), "Configurações iOS");
         nav.findElement(By.cssSelector(".nav-category-body > .nav-item:nth-child(2) > a > .nav-item-label")).click();
         Assert.assertEquals(nav.findElement(By.cssSelector("h3:nth-child(1)")).getText(), "APNS");
-        //Logoff
-        nav.findElement(By.cssSelector(".dropdown:nth-child(4) > a svg")).click();
-        nav.findElement(By.linkText("Sair")).click();
-        //
-
-
-
-
-
-
-
     }
 }
